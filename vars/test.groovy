6b055c5d3f647e7394907b57a0814058440b3589
kiran1616 <kirankumar7163@gmail.com>
2023-01-27 23:52:29 +0530

    updated script

--- vars/test.groovy
+++
@@ -1,7 +0,0 @@
-def abc = "hello"
-def xyz = 10
-
-print "abc = ${abc}"
-print "xyz = ${xyz}"
-
-
---
+++ vars/test1.groovy
@@ -0,0 +1,5 @@
+def new1() {
+    print "hello"
+    def xyz = "hello"
+    print "xyz = ${xyz}"
+    print "abc = ${abc}"
