2c016852b6c555f49059052a784aaf7a1ad066f5
kiran1616 <kirankumar7163@gmail.com>
2023-01-30 23:24:15 +0530

    updated script

--- vars/common.groovy
+++ vars/common.groovy
@@ -8,3 +8,24 @@ def compile() {
   }
 
 }
+
+def unittests() {
+
+  if (app_lang == "nodejs") {
+    // Developer is missing unit test cases in our project, He need to add them as best practice, We are skipping to proceed further
+    sh 'npm test || true'
+
+  }
+
+  if (app_lang == "maven") {
+    sh 'mvn test'
+  }
+
+  if (app_lang == "python") {
+    sh 'python3 -m unittest'
+  }
+}
+
+def email(email_note) {
+  mail bcc: '', body: "Job Failed - ${JOB_BASE_NAME}\nJenkins URL - ${JOB_URL}", cc: '', from: 'raghuk.vit@gmail.com', replyTo: '', subject: "Jenkins Job Failed - ${JOB_BASE_NAME}", to: 'raghuk.vit@gmail.com'
+}
