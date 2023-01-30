aa624a9542ec9241da266cab10973a6d10f65ee4
kiran1616 <kirankumar7163@gmail.com>
2023-01-30 23:22:02 +0530

    updated script

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,10 +1,10 @@
 def compile() {
-    if (app_lang == "nodejs") {
-        sh 'npm install'
-    }
+  if (app_lang == "nodejs") {
+    sh 'npm install'
+  }
 
-    if (app_lang == "maven") {
-        sh 'mvn package'
-    }
+  if (app_lang == "maven") {
+    sh 'mvn package'
+  }
 
 }
