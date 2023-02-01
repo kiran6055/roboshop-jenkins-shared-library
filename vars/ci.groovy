d36bf7572f9dc8a8cb410e1055b0ce19f952d81a
kiran1616 <kirankumar7163@gmail.com>
2023-02-01 16:52:33 +0530

    updated script

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -26,6 +26,10 @@ def call() {
                 }
 
                 stage('Quality Control') {
+                    environment {
+                        SONAR_USER = 'S( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        SONAR_PASS = '$( script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
+                        }
                     steps {
                         sh 'sonar-scanner -Dsonar.host.url=http://172.31.2.94:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=cart'
                     }
