2be9d2531ba7d048ff73284e1d143024a6acead9
kiran1616 <kirankumar7163@gmail.com>
2023-02-01 17:13:16 +0530

    updated script

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -32,7 +32,7 @@ def call() {
                     }
                     steps {
 
-                        sh "sonar-scanner -Dsonar.host.url=http://172.31.2.94:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=$SONAR_PASSWORD} -Dsonar.projectKey=cart"
+                        sh "sonar-scanner -Dsonar.host.url=http://172.31.2.94:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=$SONAR_PASS} -Dsonar.projectKey=cart"
                     }
                 }
 
