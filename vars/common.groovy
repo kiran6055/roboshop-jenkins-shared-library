65b0e975fb108fc4c9c50e1055a72a222bc9cc6a
kiran1616 <kirankumar7163@gmail.com>
2023-02-02 13:36:37 +0530

    updated script

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,29 +1,21 @@
 def compile() {
   if (app_lang == "nodejs") {
     sh 'npm install'
-
   }
 
   if (app_lang == "maven") {
     sh 'mvn package'
   }
 
-
-  if (app_lang == "golang") {
-    sh 'go mod init dispatch'
-    sh 'go get'
-    sh 'go build'
-
-  }
-
 }
 
 def unittests() {
-  if (app_lang == "nodejs") {
-    // developer is ,missing testcases in our projecct, so we are skipping test cases
-    sh 'npm test || true'
-  }
 
+  if (app_lang == "nodejs") {
+    // Developer is missing unit test cases in our project, He need to add them as best practice, We are skipping to proceed further
+    sh 'npm test || true'
+
+  }
 
   if (app_lang == "maven") {
     sh 'mvn test'
@@ -32,13 +24,8 @@ def unittests() {
   if (app_lang == "python") {
     sh 'python3 -m unittest'
   }
-
-  if (app-lang == "golang") {
-    sh 'go test'
-  }
-
 }
 
 def email(email_note) {
-  mail bcc: '', body: "jobfailed - ${JOB_BASE_NAME}\n jenkins-url -${JOB_URL}", cc: '', from: 'kirankumar7163@gmail.com', replyTo: '', subject: '${component} failure check mail', to: 'kirankumar.nagaraja@gmail.com'
+  mail bcc: '', body: "Job Failed - ${JOB_BASE_NAME}\nJenkins URL - ${JOB_URL}", cc: '', from: 'kirankumar7163@gmail.com', replyTo: '', subject: "Jenkins Job Failed - ${JOB_BASE_NAME}", to: 'kirankumar.nagaraja@gmail.com'
 }
