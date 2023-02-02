8f1b62dc71df9c44afd3346006b2b2e3c8a7f720
kiran1616 <kirankumar7163@gmail.com>
2023-02-02 13:25:26 +0530

    updated script

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,31 +1,32 @@
 def call() {
   try {
     node('JenkinsAgent') {
+
       stage('checkout') {
         cleanWs()
         git branch: 'main', url: "https://github.com/kirankumar7163/${component}"
         sh 'env'
       }
+
       stage('compile/build') {
           common.compile()
       }
-      stage ('unit test') {
+
+      stage('unit test') {
           common.unittests()
       }
-      stage('qualitycontrol') {
+
+      stage('Quality Control') {
           SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
           SONAR_PASS = sh(script: 'aws ssm get-parameters --region us-east-1 --names sonarqube.password  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
           wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
               sh "sonar-scanner -Dsonar.host.url=http://172.31.2.94:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component}"
           }
       }
+
       stage('Upload Code to Centralized Place') {
         echo 'upload'
       }
-
-
-
-
 
     }
   } catch (Exception e) {
--- vars/common.groovy
+++ vars/common.groovy
@@ -1,7 +1,7 @@
 def compile() {
   if (app_lang == "nodejs") {
     sh 'npm install'
-    sh 'env'
+
   }
 
   if (app_lang == "maven") {
@@ -32,9 +32,11 @@ def unittests() {
   if (app_lang == "python") {
     sh 'python3 -m unittest'
   }
+
   if (app-lang == "golang") {
     sh 'go test'
   }
+
 }
 
 def email(email_note) {
