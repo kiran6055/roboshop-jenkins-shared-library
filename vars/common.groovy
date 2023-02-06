7fec0f4f374d2079418627a38bedc8ea08b3cc4e
kiran1616 <kirankumar7163@gmail.com>
2023-02-06 17:50:37 +0530

    updated script

--- vars/common.groovy
+++ vars/common.groovy
@@ -41,6 +41,10 @@ def artifactPush() {
     sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile ${extraFiles}"
   }
 
+  if (app_lang == "maven") {
+    sh "zip -r ${component}-${TAG_NAME}.zip * ${component}.jar VERSION ${extraFiles}"
+  }
+
 
   NEXUS_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.password  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
   NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
