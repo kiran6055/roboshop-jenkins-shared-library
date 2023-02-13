6670b464ffc22f6b5bdb68832d411909a2defb47
kiran1616 <kirankumar7163@gmail.com>
2023-02-13 19:33:27 +0530

    updated script

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -9,8 +9,22 @@ def call () {
 
     parameters {
       string(name: 'Infra env', defaultValue: '', description: 'enter env like dev or prod')
-
+    }
+    stages {
+      stage('terraform init') {
+        sh "terraform init -backend-config=env-${INFRA_ENV}/state.tfvars"
+      }
+      stage('tarraform apply') {
+        sh "terraform apply -auto-approve -var-file=env-${INFRA_ENV}/main.tfvars"
+      }
 
     }
   }
+
+  post {
+    always {
+      cleanWS()
+    }
+  }
 }
+
