922e1eaa653a0d2d7ab960d5c796cbe384d0caef
kiran1616 <kirankumar7163@gmail.com>
2023-05-08 23:20:57 +0530

    n

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -37,7 +37,7 @@ def call() {
         print SONAR_PASS
         print SONAR_USER
         wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
-          sh "sonar-scanner -Dsonar.host.url=http://172.31.29.123:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
+         // sh "sonar-scanner -Dsonar.host.url=http://172.31.29.123:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true ${SONAR_EXTRA_OPTS}"
           sh "echo Sonar Scan"
         }
       }
