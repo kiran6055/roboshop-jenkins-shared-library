e1cf2dd62d3e24c8c71ef3696191a9732aeb61b0
kiran1616 <kirankumar7163@gmail.com>
2023-05-24 17:24:10 +0530

    m

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -5,8 +5,8 @@ def call () {
     }
     parameters {
       string(name: 'APP_ENV', defaultValue: '', description: 'Enter Env like dev or prod')
-      choice(name: 'Action', choices: ['apply', 'destroy'], description: 'Enter COMPONENT name')
-      choice(name: 'APP_VERSION', choices: ['apply', 'destroy'], description: 'Enter Version number')
+      string(name: 'Action', choices: ['apply', 'destroy'], description: 'Enter COMPONENT name')
+      string(name: 'APP_VERSION', choices: ['apply', 'destroy'], description: 'Enter Version number')
     }
     stages {
 
