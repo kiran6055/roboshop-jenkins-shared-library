e59c28e65023c84c605caa96bf94bca81aef1002
kiran1616 <kirankumar7163@gmail.com>
2023-05-24 18:14:00 +0530

    m

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -8,11 +8,20 @@ def call () {
       string(name: 'Action', defaultValue: '', description: 'Enter COMPONENT name')
       string(name: 'APP_VERSION', defaultValue: '', description: 'Enter Version number')
     }
+
+    options {
+      ansiColor('xterm')
+    }
+
     stages {
       stage('Run Deployment') {
         steps {
           sh '''
             aws ssm put-parameter --name "${APP_ENV}.${component}.APP_VERSION" --type "String" --value "${APP_VERSION}" --overwrite
+            aws ec2 describe-instances  --filters "Name=tag:Name,Values=${APP_ENV}-${component}" | jq ".Reservations[].Instances[].PrivateIpAddress" >/tmp/hosts
+            
+            ansible-playbook -i /tmp/hosts deploy.yml -e component=${component} -e env=${APP_ENV}
+
           '''
         }
       }
