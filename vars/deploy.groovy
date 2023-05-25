b390d35262153ae5e1f2b25c5d2040ce2d7fddec
kiran1616 <kirankumar7163@gmail.com>
2023-05-25 12:45:11 +0530

    m

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -23,8 +23,12 @@ def call () {
         steps {
           sh '''
             aws ssm put-parameter --name "${APP_ENV}.${component}.APP_VERSION" --type "String" --value "${APP_VERSION}" --overwrite
+            #this is for immutable approach
+            aws autoscaling start-instance-refresh --auto-scaling-group-name ${env}-${component} --preferences '{"InstanceWarmup": 240, "MinHealthyPercentage": 50, "SkipMatching: false,}\'
+           
+           
+            # this is for mutable approach
             aws ec2 describe-instances  --filters "Name=tag:Name,Values=${APP_ENV}-${component}" | jq ".Reservations[].Instances[].PrivateIpAddress" >/tmp/hosts
-            
             ansible-playbook -i /tmp/hosts deploy.yml -e component=${component} -e env=${APP_ENV} -e ansible_user=${ssh_USR} -e ansible_password=${ssh_PSW}
 
           '''
