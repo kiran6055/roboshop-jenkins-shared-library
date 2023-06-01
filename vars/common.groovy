27659ec826323c981ff54b8a2c207c0e9008dd98
kiran1616 <kirankumar7163@gmail.com>
2023-06-01 16:57:11 +0530

    m

--- vars/common.groovy
+++ vars/common.groovy
@@ -12,7 +12,8 @@ def compile() {
     sh "go build"
   }
 
- sh "docker build -t ${component} ."
+  // this used to build the node JS in docker images below after -t which is copied from amazonecr like different componet start from number paster here tag-name which is already given variable
+ sh "docker build -t 086083061026.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} ."
 
 }
 
@@ -41,29 +42,34 @@ def email(email_note) {
 }
 
 def artifactPush() {
-  sh "echo ${TAG_NAME} >VERSION"
+  // this is used to push the code to amazon ecr for that we need to create a ecr in amason with component like shipping payment after that we need to copy the  1 link give below 3 link after that docker build in above copy after -t paste here to push the code
+  sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 086083061026.dkr.ecr.us-east-1.amazonaws.com"
+  sh "docker push 086083061026.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}"
 
-  if (app_lang == "nodejs") {
-    sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${extraFiles}"
-  }
+// this is used to push the articat to remote repository like sona nexus
+//  sh "echo ${TAG_NAME} >VERSION"
 
-  if (app_lang == "nginx" || app_lang == "python") {
-    sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile ${extraFiles}"
-  }
+//  if (app_lang == "nodejs") {
+//    sh "zip -r ${component}-${TAG_NAME}.zip node_modules server.js VERSION ${extraFiles}"
+//  }
+//
+//  if (app_lang == "nginx" || app_lang == "python") {
+//    sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile ${extraFiles}"
+//  }
 
-  if (app_lang == "maven") {
-    sh "zip -r ${component}-${TAG_NAME}.zip * ${component}.jar VERSION ${extraFiles}"
-  }
+//  if (app_lang == "maven") {
+//    sh "zip -r ${component}-${TAG_NAME}.zip * ${component}.jar VERSION ${extraFiles}"
+//  }
 
-  if (app_lang == "golang") {
-    sh "zip -r ${component}-${TAG_NAME}.zip * x Jenkinsfile ${extraFiles}"
-  }
+//  if (app_lang == "golang") {
+//    sh "zip -r ${component}-${TAG_NAME}.zip * x Jenkinsfile ${extraFiles}"
+//  }
 
 
-  NEXUS_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.password  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-  NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
-  wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
-    sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.12.164:8081/repository/${component}/${component}-${TAG_NAME}.zip"
-  }
+//  NEXUS_PASS = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.password  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+//  NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
+//  wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${NEXUS_PASS}", var: 'SECRET']]]) {
+//    sh "curl -v -u ${NEXUS_USER}:${NEXUS_PASS} --upload-file ${component}-${TAG_NAME}.zip http://172.31.12.164:8081/repository/${component}/${component}-${TAG_NAME}.zip"
+//  }
 
 }
